2f7ad6e1e1cc3483312694b5f2fd0243f216c479
Isaac <your.email@example.com>
2024-12-12 23:48:19 +0300

    js DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the element with the ID 'text'
+    const element = document.getElementById('text');
+  
+    // Change the text content of the selected element
+    element.textContent = 'This is really cool!';
+  });
+  
